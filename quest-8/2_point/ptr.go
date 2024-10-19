bae45c74073aa9076c5dc395b64248ec0dc055c7
el mehhdi <lmehdi0006@gmail.com>
2024-10-19 22:34:31 +0100

    pool prep

---
+++ quest-8/2_point/ptr.go
@@ -0,0 +1,14 @@
+// code li ghadi n'modifeweh
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+}
